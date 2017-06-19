0196770c3f69a050ed8efca0de3d5a3fcb2fab3f
Rodrigo Arias <kreppar@gmail.com>
2017-06-19 00:07:33 -0300

    first commit

---
+++ 0 create.sql
@@ -0,0 +1,46 @@
+-- CREACIÓN DE LA BD
+CREATE DATABASE BD_VEHICULOS
+GO
+USE BD_VEHICULOS
+GO
+-- CREACIÓN DE TABLAS
+CREATE TABLE Paises(
+  codPais character(1) NOT NULL,
+  nomPais varchar(30) NOT NULL)
+GO
+CREATE TABLE Fabricantes(
+  codFab character(2) NOT NULL,
+  nomFab varchar(30) NOT NULL,
+	dirFab varchar(30) NOT NULL,
+	mailFab varchar(50))
+GO
+CREATE TABLE Plantas(
+  codPlan numeric(6) NOT NULL,
+  codFab character(2) NOT NULL,
+	nomPlan varchar(30) NOT NULL,
+	dirPlan varchar(30) NOT NULL,
+	mailPlan varchar(50),
+	codPais character(1) NOT NULL)
+GO
+CREATE TABLE Vehiculos(
+  vin character(17) NOT NULL,
+  modelo varchar(30) NOT NULL,
+	color varchar(20) NOT NULL,
+	peso numeric(12,2),
+	caracteristicas varchar(100),
+	codPais character(1) NOT NULL,
+	codFab character(2) NOT NULL)
+GO
+CREATE TABLE Envios(
+  idEnvio numeric(6) identity(1,1) NOT NULL,
+  fchEnvio datetime,
+	pesoEnvio numeric(12,2),
+	oriEnvio character(1) NOT NULL,
+	desEnvio character(1) NOT NULL)
+GO
+CREATE TABLE Carga(
+  idEnvio numeric(6) NOT NULL,
+  idCarga numeric(6) NOT NULL,
+	vin character(17) NOT NULL,
+	pesoCarga numeric(12,2))
+GO
---
+++ 2 indices.sql
@@ -0,0 +1,18 @@
+USE BD_VEHICULOS;
+GO
+-- INDEX PLANTAS
+CREATE INDEX idx_Plantas_codFab ON Plantas(codFab);
+CREATE INDEX idx_Plantas_codPais ON Plantas(codPais);
+GO
+-- INDEX VEHÍCULOS
+CREATE INDEX idx_Vehiculos_codPais ON Vehiculos(codPais);
+CREATE INDEX idx_Vehiculos_codFab ON Fabricantes(codFab);
+GO
+-- INDEX ENVIOS
+CREATE INDEX idx_Envios_oriEnvio ON Envios(oriEnvio);
+CREATE INDEX idx_Envios_desEnvio ON Envios(desEnvio);
+CREATE INDEX idx_Envios_fchEnvio ON Envios(fchEnvio);
+GO
+-- INDEX CARGA
+CREATE INDEX idx_Carga_idCarga ON Carga(idCarga);
+CREATE INDEX idx_Carga_vin ON Carga(vin);
