662039a15b4d9bffe696d3f8fa4b397ea3feb6a5
Rodrigo Arias <kreppar@gmail.com>
2017-07-09 03:05:48 -0300

    ready function a

--- 4 funciones.sql
+++ 4 funciones.sql
@@ -1,3 +1,141 @@
+USE BD_VEHICULOS;
+GO
+--------------------------------------------------------------------------------------------------------
+-- FUNCTION A
+
+-- CREATE AUX TABLE
+CREATE TABLE ValorLetra(
+  letra character(1) NOT NULL,
+  valor int NOT NULL,
+  CONSTRAINT pk_valorLetra PRIMARY KEY (letra),
+  CONSTRAINT ck_valorLetra_letra CHECK (letra NOT LIKE '%I%' AND letra NOT LIKE '%O%' AND letra NOT LIKE '%Q%'))
+GO
+-- INSERT VALUES
+INSERT INTO ValorLetra VALUES
+  ('A',1),
+  ('B',2),
+  ('C',3),
+  ('D',4),
+  ('E',5),
+  ('F',6),
+  ('G',7),
+  ('H',8),
+  ('J',1),
+  ('K',2),
+  ('L',3),
+  ('M',4),
+  ('N',5),
+  ('P',7),
+  ('R',9),
+  ('S',2),
+  ('T',3),
+  ('U',4),
+  ('V',5),
+  ('W',6),
+  ('X',7),
+  ('Y',8),
+  ('Z',9)
+GO
+-- CREATE FUNCTION
+CREATE FUNCTION fn_valor_letra (@letra character(1))
+RETURNS int
+AS BEGIN
+	DECLARE	@valor int
+
+	SELECT @valor = valor FROM ValorLetra WHERE letra = @letra;
+	RETURN @valor;
+END
+-- CREATE AUX TABLE
+CREATE TABLE FactorMultiplicador(
+  posicion int NOT NULL,
+  multiplicador int NOT NULL,
+  CONSTRAINT pk_factorMultiplicador PRIMARY KEY (posicion),
+  CONSTRAINT ck_actorMultiplicador_posicion CHECK (posicion IN (1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17)))
+GO
+-- INSERT VALUES
+INSERT INTO FactorMultiplicador VALUES
+  (1,8),
+  (2,7),
+  (3,6),
+  (4,5),
+  (5,4),
+  (6,3),
+  (7,2),
+  (8,10),
+  (10,9),
+  (11,8),
+  (12,7),
+  (13,6),
+  (14,5),
+  (15,4),
+  (16,3),
+  (17,2)
+GO
+-- CREATE FUNCTION
+CREATE FUNCTION fn_factor_multiplicador (@posicion int)
+RETURNS int
+AS BEGIN
+	DECLARE	@factor int
+
+	SELECT @factor = multiplicador FROM FactorMultiplicador WHERE posicion = @posicion;
+	RETURN @factor;
+END
+-- CREATE FUNCTION
+CREATE FUNCTION fn_validar_vin (@vin character(17))
+RETURNS character(17)
+AS BEGIN
+	DECLARE
+  @subtotal int,
+  @resto int,
+  @pos int,
+  @verificador char(1),
+  @char char(1),
+  @valorLetra int,
+  @multiplicador int,
+  @retorno varchar(17)
+
+  SET @subtotal = 0
+  SET @verificador = Substring(@vin, 9, 1)
+  SET @pos = 1
+
+  WHILE  @pos <= LEN (@vin)
+  	BEGIN
+  		SELECT @char = Substring(@vin, @pos, 1)
+
+  		IF (ISNUMERIC(@char) = 1)
+  			BEGIN
+  				SET @valorLetra = @char
+  			END
+  		ELSE
+  			BEGIN
+  				EXEC @valorLetra = dbo.fn_valor_letra @char
+  			END
+  		IF (@pos !=9)
+  			BEGIN
+  				EXEC @multiplicador = dbo.fn_factor_multiplicador @pos
+  				SET @subtotal = @subtotal + @valorLetra * @multiplicador
+  			END
+  		SET @pos = @pos + 1
+  	END
+
+  SET @resto = @subtotal % 11
+
+  DECLARE @aux char(1)
+  SET @aux = CONVERT(char(1), @resto)
+
+  IF(@aux = @verificador)
+  	BEGIN
+  		SET @retorno = 'OK'
+  	END
+  ELSE
+  	BEGIN
+  		SET @retorno = STUFF(@vin, 9, 1, @resto);
+  	END
+
+  RETURN @retorno
+END
+
+--------------------------------------------------------------------------------------------------------
 -- FUNCTION B
 
 USE BD_VEHICULOS
@@ -73,13 +211,118 @@ INSERT INTO AnioModelo VALUES
   ('2039','9')
 GO
 -- CREATE FUNCTION
-CREATE FUNCTION fn_AnioVehiculo (@vin character(17))
+CREATE FUNCTION fn_anio_vehiculo (@vin character(17))
 RETURNS int
 AS BEGIN
 	DECLARE	@anio int, @caracter char(1)
 
 	SELECT @caracter = SUBSTRING(@vin, 10, 1)
 	SELECT @anio = anio FROM AnioModelo WHERE valor = @caracter;
-
 	RETURN @anio;
 END
+--------------------------------------------------------------------------------------------------------
+-- FUNCTION C
+go
+CREATE FUNCTION FN_MayorCantVehicDePais(@fecha1 DATE, @fecha2 DATE)
+RETURNS INT
+AS
+BEGIN
+	 DECLARE @cant INT;
+
+     SELECT @cant = COUNT(V.vin)
+	 FROM Vehiculos V INNER JOIN Carga C
+	 ON V.vin = C.vin INNER JOIN Envios E
+	 ON C.idEnvio  = E.idEnvio INNER JOIN Paises P
+	 ON E.desEnvio = P.codPais
+	 WHERE E.fchEnvio BETWEEN @fecha1 AND @fecha2
+	 GROUP BY P.codPais
+	 HAVING COUNT(V.vin) >= ALL (SELECT COUNT(V.vin)
+								 FROM Vehiculos V INNER JOIN Carga C
+								 ON V.vin = C.vin INNER JOIN Envios E
+								 ON C.idEnvio  = E.idEnvio INNER JOIN Paises P
+								 ON E.desEnvio = P.codPais
+								 GROUP BY P.nomPais
+								 )
+
+	 RETURN @cant;
+END;
+
+--****************************************
+--EJECUCI�N DE "FN_MayorCantVehicDePais"
+--****************************************
+go
+Declare @fecha1 DATE
+Set @fecha1 = '2013/05/06'
+
+Declare @fecha2 DATE
+Set @fecha2 = '2018/06/07'
+
+DECLARE @retorno INT
+EXEC @retorno = dbo.FN_MayorCantVehicDePais @fecha1, @fecha2;
+
+PRINT 'La mayor cantidad de veh�culos que recibi� un pa�s entre la fecha ' + convert(varchar(10), @fecha1)  + ' y ' +  convert(varchar(10), @fecha2) + ' es: ' + convert(varchar(10), @retorno);
+
+--------------------------------------------------------------------------------------------------------
+-- FUNCTION D
+
+--------------------------------------------------------------------------------------------------------
+-- FUNCTION E
+
+--E)
+--PENDIENTE ==> SOLUCIONAR PARA QUE ENVIE VARIOS RESULTADOS.
+go
+CREATE FUNCTION FN_PaisConMenosEnvios(@fecha1 DATE, @fecha2 DATE)
+RETURNS VARCHAR(30)
+AS
+BEGIN
+	 DECLARE @pais VARCHAR(1500);
+
+	 SELECT @pais = COALESCE(@pais,' ') + P.nomPais + ' ; '
+	 FROM Vehiculos V INNER JOIN Carga C
+	 ON V.vin = C.vin INNER JOIN Envios E
+	 ON C.idEnvio  = E.idEnvio INNER JOIN Paises P
+	 ON E.desEnvio = P.codPais
+	 WHERE E.fchEnvio BETWEEN @fecha1 AND @fecha2
+	 GROUP BY  P.nomPais
+	 HAVING COUNT(V.vin) <= ALL (SELECT COUNT(V.vin)
+								 FROM Vehiculos V INNER JOIN Carga C
+								 ON V.vin = C.vin INNER JOIN Envios E
+								 ON C.idEnvio  = E.idEnvio INNER JOIN Paises P
+								 ON E.desEnvio = P.codPais
+								 GROUP BY P.nomPais)
+
+	 RETURN @pais;
+END;
+
+--****************************************
+--EJECUCI�N DE "FN_PaisQueRecibioMasVehic"
+--****************************************
+go
+Declare @fecha1 DATE
+Set @fecha1 = '2013/05/06'
+
+Declare @fecha2 DATE
+Set @fecha2 = '2018/06/07'
+
+DECLARE @retorno VARCHAR(1500);
+
+eXEC @retorno = dbo.FN_PaisConMenosEnvios @fecha1, @fecha2;
+
+PRINT 'El pa�s al cual se hizo menos cantidad de envios entre la fecha ' + convert(varchar(10), @fecha1)  + ' y ' +  convert(varchar(10), @fecha2) + ' es: ' + convert(varchar(1500), @retorno);
+
+--------------------------------------------------------------------------------------------------------
+-- FUNCTION F
+
+--F)
+--FALTA MEJORAR. MUESTRA LAS DOS COLUMNAS IGUALES.
+go
+CREATE PROCEDURE SP_cantPlantasYvehic @codFabricante CHARACTER(2), @cantPlantas INT OUTPUT, @cantModVehic INT OUTPUT
+AS
+BEGIN
+	SELECT COUNT(V.vin), COUNT(P.codPlan)
+	FROM Fabricantes F INNER JOIN Plantas P
+	ON F.codFab = P.codFab INNER JOIN Vehiculos V
+	ON F.codFab = V.codFab
+	WHERE	F.codFab = @codFabricante
+END
+--------------------------------------------------------------------------------------------------------
--- 6 consultas.sql
+++ 6 consultas.sql
@@ -1,36 +1,35 @@
 USE BD_VEHICULOS;
 GO
-
+--------------------------------------------------------------------------------------------------------
 -- QUERY A
 SELECT MAX(pesoEnvio) AS  'Máximo Peso del Período', MIN(pesoEnvio) AS 'Mínimo Peso del Período', MAX(fchEnvio) AS 'Mayor Fecha del Período', MIN(fchEnvio) AS 'Menor Fecha del Período'
 FROM Envios
 WHERE YEAR(fchEnvio) IN (2015,2016)
-
+--------------------------------------------------------------------------------------------------------
 -- QUERY B
 SELECT F.nomFab AS 'Nombre del Fabricante', COUNT(nomFab) AS 'Vehiculos Enviados', SUM(V.peso) AS 'Peso Total en 2016'
 FROM Fabricantes F, Vehiculos V, Carga C, Envios E
 WHERE F.codFab = V.codFab AND V.vin = C.vin AND E.idEnvio = C.idEnvio AND YEAR(E.fchEnvio) = 2016
 GROUP BY F.nomFab
 ORDER BY SUM(V.peso) DESC
-
+--------------------------------------------------------------------------------------------------------
 -- QUERY C
 SET DATEFORMAT DMY
-SELECT nomPais 'Nombre de País',
-(SELECT COUNT(desEnvio)
- FROM Envios
- WHERE Paises.codPais = Envios.desEnvio AND Envios.fchEnvio BETWEEN '01/01/2016' AND '20/01/2016'
- GROUP BY desEnvio) AS 'Cant. de Envíos',
-(SELECT MAX(Envios.fchEnvio)
- FROM Envios
- WHERE Paises.codPais = Envios.desEnvio AND Envios.fchEnvio BETWEEN '01/01/2016' AND '20/01/2016') AS 'Última Fecha Envío'
+SELECT nomPais 'Nombre de País', (SELECT COUNT(desEnvio)
+                                  FROM Envios
+                                  WHERE Paises.codPais = Envios.desEnvio AND Envios.fchEnvio BETWEEN '01/01/2016' AND '20/01/2016'
+                                  GROUP BY desEnvio) AS 'Cant. de Envíos',
+                                 (SELECT MAX(Envios.fchEnvio)
+                                  FROM Envios
+                                  WHERE Paises.codPais = Envios.desEnvio AND Envios.fchEnvio BETWEEN '01/01/2016' AND '20/01/2016') AS 'Última Fecha Envío'
 FROM Paises
-
+--------------------------------------------------------------------------------------------------------
 -- QUERY E
 SELECT V.*
 FROM Vehiculos V, Carga C, Envios E
 WHERE V.vin = C.vin AND C.idEnvio  = E.idEnvio AND E.fchEnvio = (SELECT MAX(fchEnvio)
                                                                  FROM Envios)
-
+--------------------------------------------------------------------------------------------------------
 -- QUERY F
 SET DATEFORMAT DMY
 SELECT F.*
@@ -44,7 +43,7 @@ AND F.codFab IN (SELECT F.codFab
 AND F.codFab IN (SELECT F.codFab
                FROM Vehiculos V, Carga C, Envios E, Paises P
                WHERE F.codFab = V.codFab AND V.vin = C.vin AND E.idEnvio = C.idEnvio AND P.codPais = E.desEnvio AND E.nomPais = 'Holanda')
-
+--------------------------------------------------------------------------------------------------------
  -- QUERY G
  SELECT V.vin, V.modelo, V.color, V.peso, V.caracteristicas, V.codPais, V.codFab, MAX(E.fchEnvio) AS 'fechaUlt.Envío', F.nomFab
  FROM Fabricantes F, Vehiculos V, Carga C, Envios E
@@ -52,9 +51,7 @@ AND F.codFab IN (SELECT F.codFab
  V.vin IN (SELECT V.vin
            WHERE V.peso < 2300)
  GROUP BY V.vin, V.modelo, V.color, V.peso, V.caracteristicas, V.codPais, V.codFab, F.nomFab
-
 --------------------------------------------------------------------------------------------------------
-
 -- QUERY D
 -- ******************** INCOMPLETO ********************
 SELECT *
@@ -63,26 +60,34 @@ WHERE codFab IN (SELECT F.codFab
                  FROM Fabricantes F, Vehiculos V, Carga C, Envios E
                  WHERE F.codFab = V.codFab AND V.vin = C.vin AND E.idEnvio = C.idEnvio
                  GROUP BY F.codFab, E.idEnvio
-                 HAVING COUNT(C.vin) > 0) AND
+                 HAVING COUNT(C.vin) > 500) AND
   codFab NOT IN (SELECT F.codFab
                  FROM Fabricantes F, Vehiculos V, Carga C, Envios E
                  WHERE F.codFab = V.codFab AND V.vin = C.vin AND E.idEnvio = C.idEnvio
                  GROUP BY F.codFab, E.idEnvio
-                 HAVING COUNT(C.vin) < 1)
+                 HAVING COUNT(C.vin) < 100)
 
 -- ******************** INCOMPLETO ********************
-
 --------------------------------------------------------------------------------------------------------
-
 -- QUERY H
---UPDATE Vehiculos SET peso = peso - peso * 0.05
---WHERE
-
--- QUERY I
-
+UPDATE Vehiculos
+SET peso = peso - peso * 0.05
+WHERE vin NOT IN (SELECT V.vin
+                  FROM Vehiculos V, Carga C, Envios E
+                  WHERE V.vin = C.vin AND C.idEnvio = E.idEnvio AND YEAR(fchEnvio) >= dbo.fn_anio_vehiculo(V.vin) + 1)
 --------------------------------------------------------------------------------------------------------
-
---------------------------------------------------
-SELECT STUFF('abcdef', 2, 3, 'ijklmn');
-GO
---------------------------------------------------
+-- QUERY I
+SELECT Fabricantes.*
+FROM Fabricantes, Plantas, Paises
+WHERE Fabricantes.codFab = Plantas.codFab AND Plantas.codPais = Paises.codPais AND Paises.nomPais = 'Japón' AND
+Fabricantes.codFab IN (SELECT F.codFab
+					   FROM Fabricantes F, Vehiculos V, Carga C, Envios E
+					   WHERE F.codFab = V.codFab AND V.vin = C.vin AND E.idEnvio = C.idEnvio AND YEAR(E.fchEnvio) = 2016
+					   GROUP BY F.codFab, MONTH(E.fchEnvio)
+					   HAVING COUNT(C.vin) > 100) AND
+Fabricantes.codFab IN (SELECT F.codFab
+					   FROM Fabricantes F, Vehiculos V, Carga C, Envios E
+					   WHERE F.codFab = V.codFab AND V.vin = C.vin AND E.idEnvio = C.idEnvio AND YEAR(E.fchEnvio) = 2016
+					   GROUP BY F.codFab, MONTH(E.fchEnvio)
+					   HAVING COUNT(C.vin) < 20)
+--------------------------------------------------------------------------------------------------------
